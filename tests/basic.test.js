9c27238db9e849b2b412ce7ad5709205dbeafdba
Alex Lajeunesse <alexinlife.exe@gmail.com>
2025-11-05 09:40:48 -0500

    fix part 4

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -1,8 +1,8 @@
 const { greet } = require("../src/index.js");
 
 function testGreet() {
-  const result = greet("ASDF");
-  if (!result.includes("Bonjour Monde !")) {
+  const result = greet("Alex");
+  if (!result.includes("Bonjour Alex !")) {
     console.error(`❌ Test échoué: résultat = "${result}"`);
     process.exit(1);
   } else {
